0eaaaf48e0e579a57dd1fd553f7198ebf9179d92
hello-world-cn <seuwsk@gmail.com>
2020-10-13 16:32:01 +0800

    1. Finish SpringCloud!

---
+++ SpringCloud-Config-server-3344/src/main/java/com/wang/sringcloud/Config_Server_3344.java
@@ -0,0 +1,14 @@
+package com.wang.sringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+//开启Config Server
+@EnableConfigServer
+public class Config_Server_3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(Config_Server_3344.class, args);
+    }
+}
